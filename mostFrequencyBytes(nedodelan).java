8a61f1580d3ad0bd4308c43f6b7abe669018c14c
andreynikonov23 <76613379+andreynikonov23@users.noreply.github.com>
2022-04-25 13:14:48 +0300

    Create mostFrequencyBytes(nedodelan).java

---
+++ mostFrequencyBytes(nedodelan).java
@@ -0,0 +1,63 @@
+package nick.pack;
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    /*
+    1. Ввести с консоли имя файла
+    2. Найти байт или байты с максимальным количеством повторов
+    3. Вывести их на экран через пробел.
+    4. Закрыть поток ввода-вывода
+     */
+    public static void main(String[] args){
+        String path = "C:/test/file.txt";
+        ArrayList<Integer> bytes = new ArrayList<>();
+        try(FileInputStream inputStream = new FileInputStream(path)){
+            while (inputStream.available() > 0){
+                bytes.add(inputStream.read());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(bytes + " -- size: " + bytes.size());
+        int count = 1;
+        ArrayList<Integer> counts = new ArrayList<>();
+        ArrayList<Integer> repeatByte = new ArrayList<>();
+        for (int i = 0; i < bytes.size(); i++) {
+            for (int j = 0; j < bytes.size(); j++) {
+                if (bytes.get(i) == (bytes.get(j)) && i != j) {
+                    count++;
+                }
+                if (j == bytes.size() -1 && count > 1){
+                    counts.add(count);
+                    repeatByte.add(bytes.get(i));
+                    count = 1;
+                }
+            }
+        }
+
+        System.out.println(counts + " -- size: " + counts.size());
+        System.out.println(repeatByte + " -- size: " + repeatByte.size());
+        int index = 0;
+        for (int i = 0; i < repeatByte.size(); i++) {
+            for (int j = 1; j < repeatByte.size(); j++) {
+                System.out.println(repeatByte.get(i) + " " + repeatByte.get(j));
+                if (bytes.get(i) == (bytes.get(j))){
+                    System.out.println("delete");
+                    repeatByte.remove(j);
+                    counts.remove(j);
+                }
+            }
+        }
+        System.out.println(counts + " -- size: " + counts.size());
+        System.out.println(repeatByte + " -- size: " + repeatByte.size());
+    }
+}
