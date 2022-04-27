f691762681c14f97f503a42c725665dcfb4e2043
andreynikonov23 <76613379+andreynikonov23@users.noreply.github.com>
2022-04-27 21:30:18 +0300

    Update searchInFile.java

--- searchInFile.java
+++ searchInFile.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     /*
@@ -12,21 +14,14 @@ public class Main {
     2. Найти в файле информацию, которая относится к заданному id, и вывести ее на экран в виде, в котором она записана в файле.
     3. Программа запускается с одним параметром: id (int)
     4. Закрыть потоки. Не использовать try-with-resources
-
     В файле данные разделены пробелом и хранятся в следующей последовательности:
     id productName price quantity
-
     где id - int
     productName - название товара, может содержать пробелы, String
     price - цена, double
     quantity - количество, int
-
     Информация по каждому товару хранится в отдельной строке
      */
-    private int id;
-    private String productName;
-    private double price;
-    private int quantity;
     public static void main(String[] args) throws IOException {
         String file1 = "C:/test/file.txt";
         FileInputStream inputStream = new FileInputStream(file1);
@@ -36,6 +31,27 @@ public class Main {
         }
         inputStream.close();
         String str = new String(bytes, StandardCharsets.UTF_8);
-        System.out.println(str);
+        ArrayList<String> arrayList = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(str);
+        while (tokenizer.hasMoreTokens()){
+            arrayList.add(tokenizer.nextToken("\n"));
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите id");
+        int id = Integer.parseInt(scanner.nextLine());
+        String strId = String.valueOf(id);
+        boolean isFalse = false;
+        for (int i = 0; i < arrayList.size(); i++) {
+            int firstSpace = arrayList.get(i).indexOf(" ");
+            String idStr = arrayList.get(i).substring(0, firstSpace);
+            String value = arrayList.get(i).substring(firstSpace + 1);
+            if (strId.equals(idStr)){
+                System.out.println(value);
+                isFalse = true;
+            }
+        }
+        if (isFalse == false){
+            System.out.println("Ничего не найдено");
+        }
     }
 }
